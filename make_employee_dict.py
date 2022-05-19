2b2c8e7772b13797b9094e2bf8cd4b3c860f18d6
KaiGeller <gellerka@oregonstate.edu>
2022-05-18 21:38:49 -0700

    1

--- make_employee_dict.py
+++ make_employee_dict.py
@@ -5,13 +5,13 @@
 class Employee:
     _name=""
     _ID_number=""
-    _sals=0
+    _salary=0
     _emails=""
     def __init__(self,emp_names,emp_ids,emp_sals,emp_emails):
         """This will create an employee object"""
         self._name = emp_names
         self._ID_number = emp_ids
-        self._sals = emp_sals
+        self._salary = emp_sals
         self._emails = emp_emails
     def get_name(self):
         """This will return the employee name"""
@@ -21,7 +21,7 @@ class Employee:
         return self._ID_number
     def get_salary(self):
         """This will return the employee salary"""
-        return self._sals
+        return self._salary
     def get_email_address(self):
         """This will return the employee email address"""
         return self._emails
