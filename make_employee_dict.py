76d94fa1ae943572cc2c52cef934ecc7a0f5c63d
KaiGeller <gellerka@oregonstate.edu>
2022-05-18 21:25:43 -0700

    1

--- make_employee_dict.py
+++ make_employee_dict.py
@@ -5,22 +5,22 @@
 class Employee:
     def __init__(self,emp_names,emp_ids,emp_sals,emp_emails):
         """This will create an employee object"""
-        self.__name = emp_names
-        self.__ids = emp_ids
-        self.__sals = emp_sals
-        self.__emails = emp_emails
+        self._name = emp_names
+        self._ids = emp_ids
+        self._sals = emp_sals
+        self._emails = emp_emails
     def get_name(self):
         """This will return the employee name"""
-        return self.__name
+        return self._name
     def get_ID_number(self):
         """This will return the employee ID number"""
-        return self.__ids
+        return self._ids
     def get_salary(self):
         """This will return the employee salary"""
-        return self.__sals
+        return self._sals
     def get_email_address(self):
         """This will return the employee email address"""
-        return self.__emails
+        return self._emails
 def make_employee_dict(names,ids,sals,emails):
     """This will make a dictionary with the employees information"""
     employee_dict={}
